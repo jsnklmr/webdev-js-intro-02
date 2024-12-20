88cf6d6c40b5b5af603bb48825281b037ab60609
jsnklmr <91213802+jsnklmr@users.noreply.github.com>
2024-12-19 23:03:36 -0500

    complete task part 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 // Prevent us from attempting to use variables
 // that are not declared
+/*
 "use strict"
+const myName = "Dan";
+console.log(myName)             // Output: "Dan"
+console.log(typeof myName)      // Output: "string"
 
+const one = 1;
+console.log(one)                // Output: 1
+console.log(typeof one);        // Output: "number"
+
+let isMentor = true;            
+console.log(isMentor)           // Output: true
+console.log(typeof isMentor)    // Output: "boolean"
+
+isMentor = false;
+
+console.log(isMentor)           // Output: false
+console.log(typeof isMentor)    // Output: "boolean"
+*/
