6827a136a1f35587d9358e0ad96fc64bd9d0190b
jsnklmr <91213802+jsnklmr@users.noreply.github.com>
2024-12-19 23:15:28 -0500

    part 2 tasks complete

--- js/assignment.js
+++ js/assignment.js
@@ -8,23 +8,31 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
-
+const resolution = "do better";
+const year = 2025
+let willMeetResolution = "yes";
 function updateYear() {
     // write the logic
-
+    yearElement.innerText = year;
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
+}
 
+function updateWillMeetResolution() {
+    willMeetResolutionElement.innerText = willMeetResolution ? "Yes" : "No"; // Displays "Yes" or "No"
 }
 
 function render() {
     // Finish writing this function
-
+    updateYear(); // Updates the year
+    updateResolution(); // Updates the resolution
+    updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-
+    render();
 })
